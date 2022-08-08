11f097810669dc9a02105e50f9fd73cb4834d89e
Kornel <89316637+korneliuDev@users.noreply.github.com>
2022-08-08 20:33:28 +0200

    Add files via upload

---
+++ LittleVirus2.py
@@ -0,0 +1,6 @@
+import random, pyautogui as pyauto
+for i in range(15):
+    h = random.randint(0, 1080)
+    w = random.randint(0, 1920)
+    pyauto.click(h, w, duration = 0.3)
+    pyauto.hotkey('winleft', 'm')
